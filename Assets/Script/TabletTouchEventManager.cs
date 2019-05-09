c27fb9dfc752e461c75906d40416905f84103883
lekd <duylekhanh@gmail.com>
2019-05-09 18:10:51 +0200

    Move virtual pad by gaze in VirtualPadController. Added TabletTouchEventManager to receive touch interaction from tablet

---
+++ Assets/Script/TabletTouchEventManager.cs
@@ -0,0 +1,50 @@
+﻿using Assets;
+using Assets.Script;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabletTouchEventManager : MonoBehaviour
+{
+    WebSocketSharp.WebSocket wsClient;
+    // Start is called before the first frame update
+    void Start()
+    {
+        connectWebSocketServer();
+    }
+    void connectWebSocketServer()
+    {
+        wsClient = new WebSocketSharp.WebSocket(string.Format("ws://{0}/main.html", GlobalUtilities.LoadServerAddress()));
+        wsClient.OnMessage += WsClient_OnMessage;
+        wsClient.Connect();
+    }
+    private void WsClient_OnMessage(object sender, WebSocketSharp.MessageEventArgs e)
+    {
+        if (e.IsText)
+        {
+            string msg = e.Data;
+            //Debug.Log(msg);
+            try
+            {
+                //Debug.Log(msg);
+                TouchEventData touchEvent = TouchEventData.ParseToucEventFromJsonString(msg);
+                //string decodedMsg = "From JSON: ";
+                //decodedMsg += string.Format("Event type:{0};Pointers count: {1};AvaiPointers:{2}", touchEvent.EventType, touchEvent.PointerCount, touchEvent.AvaiPointers.Length);
+                
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("JSON Parsing Error:" + ex.Message);
+            }
+        }
+    }
+    void OnApplicationQuit()
+    {
+        if (wsClient != null)
+        {
+            wsClient.Close();
+            wsClient = null;
+        }
+    }
+}
--- Assets/Script/VirtualPadController.cs
+++ Assets/Script/VirtualPadController.cs
@@ -1,47 +1,56 @@
-﻿using System;
+﻿using Assets.Script;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VirtualPadController : MonoBehaviour
 {
+    const int MAX_FINGERS_COUNT = 5;
+    const float VIRTUALPAD_DEPTHOFFSET = -0.01f;
     //Public Game Objects
     public GameObject eventListennerObject;
     public GameObject gameCameraObject;
     public GameObject gazePointObject;
+    public GameObject boardObject;
+    public GameObject finger1;
+    public GameObject finger2;
+    public GameObject finger3;
+    public GameObject finger4;
+    public GameObject finger5;
+    GameObject[] fingers;
 
-    // 2D Coordinates of the Center of the Virtual Pad
-    Vector2 center = Vector2.zero; //Center point of the virtual pad
-
-    Vector2 A = Vector2.zero; // Top left point
-    Vector2 B = Vector2.zero; // Top right point
-    Vector2 C = Vector2.zero; // Bottom right point
-    Vector2 D = Vector2.zero; // Bottom left point
+    Bounds boardObjectBound;
 
     // Variables for timer;
     float translationTime = 0f;
     Vector2 translationVector = Vector2.zero;
-
-    float rotationTime = 0f;
-    float rotationAngle = 0f;
-    float rotationStartTime = 0f;
 
     Camera gameCamera;
     Transform gazePoint;
 
     void Start()
     {
-        if(gameCameraObject)
+        if (gameCameraObject)
+        {
             gameCamera = gameCameraObject.GetComponent<Camera>();
+        }
 
         if (gazePointObject)
             gazePoint = gazePointObject.GetComponent<Transform>();
+
+        boardObjectBound = boardObject.GetComponent<Collider>().bounds;
+        fingers = new GameObject[] { finger1, finger2, finger3, finger4, finger5 };
+        for(int i=0; i< fingers.Length; i++)
+        {
+            fingers[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(center);
+        /*Debug.Log(center);
 
         // Translate the pad to a new position
         if(translationTime > 0)
@@ -52,7 +61,7 @@ public class VirtualPadController : MonoBehaviour
         else if(translationTime < 0)
         {
             translationTime = 0;
-        }
+        }*/
 
 
         //Update Virtual Pad Following Camera Lookat Vector
@@ -61,137 +70,32 @@ public class VirtualPadController : MonoBehaviour
 
     private void UpdateVirtualPadBasedOnCamera()
     {
-        if(gameCamera)
+
+        if (!gameCamera)
         {
-            //Vector2 cameraPosition = new Vector2(gameCamera.transform.position.z, gameCamera.transform.position.y);
-            //Vector2 virtualPadPosition = new Vector2(this.transform.position.z, gameCamera.transform.position.y);
-            //Vector2 lookAt = new Vector2(gazePoint.position.z - gameCamera.transform.position.z, gazePoint.position.y - gameCamera.transform.position.z);
+            return;
+        }
+        Bounds virtualPadBound = gameObject.GetComponent<Collider>().bounds;
+        Rect pad2DContainerLimit = new Rect();
+        pad2DContainerLimit.xMin = boardObjectBound.min.x + virtualPadBound.size.x / 2;
+        pad2DContainerLimit.xMax = boardObjectBound.max.x - virtualPadBound.size.x / 2;
+        pad2DContainerLimit.yMin = boardObjectBound.min.y + virtualPadBound.size.y / 2;
+        pad2DContainerLimit.yMax = boardObjectBound.max.y - virtualPadBound.size.y / 2;
 
-            //float angle = Vector2.Angle(virtualPadPosition - cameraPosition, lookAt);
-
-            //float b = (virtualPadPosition - cameraPosition).magnitude * Mathf.Sin(angle) / Mathf.Sin(90 - angle);
-
-            //float x = 
+        RaycastHit hitInfo;
+        Ray camRay = new Ray(gazePointObject.transform.position, gazePointObject.transform.position - gameCamera.transform.position);
+        if (Physics.Raycast(camRay, out hitInfo))
+        {
+            if (hitInfo.collider != null)
+            {
+                if (hitInfo.collider.name.CompareTo(boardObject.name) == 0)
+                {
+                    Vector3 hitPos = hitInfo.point;
+                    Vector3 curPadPos = gameObject.transform.position;
+                    Vector3 newPadPos = GlobalUtilities.boundPointToContainer(hitPos, pad2DContainerLimit);
+                    gameObject.transform.Translate(new Vector3(newPadPos.x - curPadPos.x, newPadPos.y - curPadPos.y, newPadPos.z - curPadPos.z + VIRTUALPAD_DEPTHOFFSET));
+                }
+            }
         }
     }
-
-    /// <summary>
-    /// Init the Virtual Pad
-    /// </summary>
-    /// <param name="centerPoint">The center point of the pad</param>
-    /// <param name="a">Top left position</param>
-    /// <param name="b">Top right position</param>
-    /// <param name="c">Bottom right position</param>
-    /// <param name="d">Bottom left position</param>
-    public void Init(Vector2 centerPoint, Vector2 a, Vector2 b, Vector2 c, Vector2 d)
-    {
-        center = centerPoint;
-        A = a;
-        B = b;
-        C = c;
-        D = d;
-    }
-
-    /// <summary>
-    /// Set the center of the virtual pad
-    /// </summary>
-    /// <param name="c">New center point</param>
-    public void SetCenter(Vector2 c)
-    {
-        center = c;
-    }
-
-    /// <summary>
-    /// Set the boundaries of the virtual pad
-    /// </summary>
-    /// <param name="a">Top left point</param>
-    /// <param name="b">Top right point</param>
-    /// <param name="c">Bottom right point</param>
-    /// <param name="d">Bottom left point</param>
-    public void SetBoundaries(Vector2 a, Vector2 b, Vector2 c, Vector2 d)
-    {
-        A = a;
-        B = b;
-        C = c;
-        D = d;
-    }
-
-    /// <summary>
-    /// Get the center of the virtual pad
-    /// </summary>
-    /// <returns></returns>
-    public Vector2 GetCenter()
-    {
-        return center;
-    }
-
-    /// <summary>
-    /// Get the boundaries of the virtual pad
-    /// </summary>
-    /// <returns></returns>
-    public Vector2[] GetBoundaries()
-    {
-        Vector2[] ret = new Vector2[4];
-        ret[0] = A;
-        ret[1] = B;
-        ret[2] = C;
-        ret[3] = D;
-
-        return ret;
-    }
-
-    /// <summary>
-    /// Translate the virtual pad to a new center in a "time" period
-    /// </summary>
-    /// <param name="newCenter"></param>
-    /// <param name="time"></param>
-    public void Translate(Vector2 newCenter, float time)
-    {
-        Debug.Log("Translate");
-        translationTime = time;
-        translationVector = newCenter - center;
-
-        // Update the position of the center and boundaries
-        center = newCenter;
-        A += translationVector;
-        B += translationVector;
-        C += translationVector;
-        D += translationVector;
-    }
-
-    /// <summary>
-    /// Rotate the virtual pad with an "angle" in a "time" period
-    /// </summary>
-    /// <param name="angle">Rotate angle</param>
-    /// <param name="time">Rotate duration</param>
-    public void Rotate(float angle, float time)
-    {
-        rotationTime = time;
-        rotationAngle = angle;
-
-        rotationStartTime = Time.time;
-    }
-
-    //void RotateBoundaries()
-    //{
-    //    FindNewPositionAfterRotating(A);
-    //    FindNewPositionAfterRotating(B);
-    //    FindNewPositionAfterRotating(C);
-    //    FindNewPositionAfterRotating(D);
-    //}
-
-    //void FindNewPositionAfterRotating(Vector2 point)
-    //{
-    //    Vector2 pointCenter = point - center;
-    //    Vector2 perpendicularVector = new Vector2(pointCenter.y, -pointCenter.x);
-    //    Quaternion rotationQuaternion = Quaternion.AngleAxis(rotationAngle, perpendicularVector);
-    //    Vector2 newPointCenter = pointCenter * rotationQuaternion;
-
-    //    float x = 0;
-    //    float y = 0;
-
-    //    y = ((pointCenter.magnitude * pointCenter.magnitude) * Mathf.Acos(rotationAngle) - pointCenter.x * x) / pointCenter.y;
-
-    //    x ^ 2 = pointCenter.magnitude * pointCenter.magnitude - y ^ 2;
-    //}
 }
