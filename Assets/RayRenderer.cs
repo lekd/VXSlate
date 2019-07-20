3121cfd24f6286f25e2e29aa1cb3ed9c50583807
Tanh Quang Tran <tqtanh@outlook.com>
2019-07-21 03:29:19 +1200

    Add Ray For Both Controller

---
+++ Assets/RayRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayRenderer : MonoBehaviour
+{
+    LineRenderer _laserLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _laserLine = GetComponent<LineRenderer>();
+        _laserLine.startWidth = 0.0025f;
+        _laserLine.endWidth = 0.01f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _laserLine.SetPosition(0, this.transform.position);
+
+        RaycastHit hitInfo = new RaycastHit();
+
+        bool hit = Physics.Raycast(this.transform.position, this.transform.rotation * Vector3.forward, out hitInfo);
+
+        if (hit)
+        {
+            _laserLine.SetPosition(1, hitInfo.point);
+        }
+    }
+}
